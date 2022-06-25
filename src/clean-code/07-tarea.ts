9ad7cd68606c12f9690cb9ea06c11fd0627e7db5
OscarAristizabalDev <oaristizabal@dataifx.com>
2022-06-24 21:15:34 -0500

    Mejoras en tarea de Aplicar el principio de responsabilidad única y Priorizar la composición frente a la herencia en las clases

--- src/clean-code/07-tarea.ts
+++ src/clean-code/07-tarea.ts
@@ -1,43 +1,55 @@
-(()=>{
+(() => {
 
     //* Aplicar el principio de responsabilidad única
     //* Priorizar la composición frente a la herencia
 
-    type HtmlType = 'input'|'select'|'textarea'|'radio';
-   
+    type HtmlType = 'input' | 'select' | 'textarea' | 'radio';
+
     class HtmlElement {
         constructor(
             public id: string,
             public type: HtmlType,
-        ) {}
+        ) { }
     }
 
-    class InputAttributes extends HtmlElement {
+    class InputAttributes {
         constructor(
             public value: string,
             public placeholder: string,
-            id: string,
+
         ) {
-            super(id, 'input');
         }
     }
 
-    class InputEvents extends InputAttributes {
-        constructor( value: string, placeholder: string, id: string ) {
-            super( value, placeholder, id );
-        }
+    class InputEvents {
 
-        setFocus() {};
-        getValue() {};
-        isActive() {};
-        removeValue() {};
+        constructor() { }
+
+        setFocus() { };
+        getValue() { };
+        isActive() { };
+        removeValue() { };
     }
 
 
     //? Idea para la nueva clase InputElement
 
-    const nameField = new InputEvents('Fernando', 'Enter first name', 'txtName');
+    class InputElement {
+        public html: HtmlElement;
+        public attributes: InputAttributes;
+        public events: InputEvents;
 
-    console.log({ nameField });
+        constructor(value: string, placeholder: string, id: string) {
+            this.html = new HtmlElement(id, 'input');
+            this.attributes = new InputAttributes(value, placeholder);
+            this.events = new InputEvents();
+        }
+    }
+
+    const nameField = new InputElement('Fernando', 'Enter first name', 'txtName');
+    nameField.events.isActive();
+
+    console.log({ nameField })
+
 
 })()
