529645610cbe5d25c3ce10b1d102c6593b488c2f
OscarAristizabalDev <oaristizabal@dataifx.com>
2022-07-07 15:06:56 -0500

    Se aplica el principio de abierto y cerrado, porque ya no es necesario hacer un if por cada nuevo carro que se adicione

--- src/solid/03-liskov-a.ts
+++ src/solid/03-liskov-a.ts
@@ -2,34 +2,38 @@ import { Tesla, Audi, Toyota, Honda, Vehicle, Ferrari } from './03-liskov-b';
 
 
 (() => {
-    
+
     //el metodo printCarSeats recibe un Array de Vehicle y de este modo nuestra funcion va a recibir cualquier
     // tipo de vehiculo sin necesidad de agregar un nuevo tipo de vehiculo por parametro
     // por lo tanto estamos aplicando el principio de sustitución de liskov
-    const printCarSeats = ( cars: Vehicle[] ) => {
-        
-        for (const car of cars) {
-         
-            if( car instanceof Tesla ) {
-                console.log( 'Tesla', car.getNumberOfSeats() )
-                continue;
-            }
-            if( car instanceof Audi ) {
-                console.log( 'Audi', car.getNumberOfSeats() )
-                continue;
-            }
-            if( car instanceof Toyota ) {
-                console.log( 'Toyota', car.getNumberOfSeats() )
-                continue;
-            }
-            if( car instanceof Honda ) {
-                console.log( 'Honda', car.getNumberOfSeats() )
-                continue;
-            }         
+    const printCarSeats = (cars: Vehicle[]) => {
 
-        }
+        cars.forEach(car => {
+            console.log(car.constructor.name, car.getNumberOfSeats());
+        })
+
+        // for (const car of cars) {
+
+        //     if( car instanceof Tesla ) {
+        //         console.log( 'Tesla', car.getNumberOfSeats() )
+        //         continue;
+        //     }
+        //     if( car instanceof Audi ) {
+        //         console.log( 'Audi', car.getNumberOfSeats() )
+        //         continue;
+        //     }
+        //     if( car instanceof Toyota ) {
+        //         console.log( 'Toyota', car.getNumberOfSeats() )
+        //         continue;
+        //     }
+        //     if( car instanceof Honda ) {
+        //         console.log( 'Honda', car.getNumberOfSeats() )
+        //         continue;
+        //     }         
+
+        // }
     }
-    
+
     const cars = [
         new Tesla(7),
         new Audi(2),
@@ -39,6 +43,6 @@ import { Tesla, Audi, Toyota, Honda, Vehicle, Ferrari } from './03-liskov-b';
     ];
 
 
-    printCarSeats( cars );
+    printCarSeats(cars);
 
 })();
